d970c324d0e25324ab21346ef7025cdc4f2f801f
brsch6643 <brsc6643@colorado.edu>
2021-03-08 23:58:56 -0700

    Milestone committed.

--- Project Code/SettingsPage/settings.js
+++ Project Code/SettingsPage/settings.js
@@ -6,3 +6,20 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://roomble-5fbeb-default-rtdb.firebaseio.com"
 });
+
+console.log("JS accessed");
+
+
+//var slider = document.getElementById("rent");
+//var output = document.getElementById("value");
+//output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = slider.value;
+}
+
+
+function outputUpdate(vol){
+  document.querySelector('#volume').value = vol;
+}
