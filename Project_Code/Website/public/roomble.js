c65d1abee8a02e69eb038a4a7cba6821f959d445
tenneyf13 <70172174+tenneyf13@users.noreply.github.com>
2021-04-12 10:59:53 -0600

    onload added

--- Project_Code/Website/public/roomble.js
+++ Project_Code/Website/public/roomble.js
@@ -1,6 +1,4 @@
-
-
-  // Your web app's Firebase configuration
+// Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   var firebaseConfig = {
     apiKey: "AIzaSyAntjt2kCb6-HpIcfmswpu7lNly-6vaItA",
@@ -20,7 +18,7 @@
 var app = firebase.initializeApp(firebaseConfig);
 //firebase.analytics();
 //var db = app.database(); //access firebase
-var db = firebase.database();
+var db = firebase.database(); //MAY NEED TO ADD AGAIN LATER
 function readURL(input, target)
 {
     document.getElementById(target).style.display = "block";
@@ -36,17 +34,17 @@ function readURL(input, target)
     }
 }
 
-document.addEventListener("DOMContentLoaded", event=>
-{
-  const app = firebase
-  console.log(app)
-  //checking if firebase is available.
-  firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      window.location = '../homeScreen/homepage.html';
-    }
-  });
-});
+// document.addEventListener("DOMContentLoaded", event=>
+// {
+//   const app = firebase
+//   console.log(app)
+//   //checking if firebase is available.
+//   firebase.auth().onAuthStateChanged(function(user) {
+//     if (user) {
+//       window.location = '../photoPage/photospage.html';
+//     }
+//   });
+// });
 
 function emailLogin() {
   const provider = new firebase.auth.GoogleAuthProvider();
@@ -57,8 +55,45 @@ function emailLogin() {
             {
               const user = result.user;
               document.write(`Hello ${user.displayName}`);
+              /*
+             //new code
+              return user.getIdToken().then(idToken = > {
+              // Session login endpoint is queried and the session cookie is set.
+              // CSRF protection should be taken into account.
+              // ...
+             const csrfToken = getCookie('csrfToken')
+             return postIdTokenToSessionLogin('/sessionLogin', idToken, csrfToken);
+            //end of new code
+            */
               //then upload photos.
-              window.location = '../homeScreen/homepage.html';
+              window.location = '../photoPage/photospage.html';
+
+
+              console.log(user)
+            })
+            .catch(console.log)
+}
+function emailLoginData() {
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  firebase.auth().signInWithPopup(provider)
+
+          .then(result =>
+            {
+              const user = result.user;
+              document.write(`Hello ${user.displayName}`);
+              /*
+             //new code
+              return user.getIdToken().then(idToken = > {
+              // Session login endpoint is queried and the session cookie is set.
+              // CSRF protection should be taken into account.
+              // ...
+             const csrfToken = getCookie('csrfToken')
+             return postIdTokenToSessionLogin('/sessionLogin', idToken, csrfToken);
+            //end of new code
+            */
+              //then upload photos.
+            //  window.location = '../photoPage/photospage.html';
 
 
               console.log(user)
@@ -67,40 +102,95 @@ function emailLogin() {
 }
 
   //function that adds data from photospage.html to database
-function addPhotoToDatabase()
-{
-  var user = firebase.auth().currentUser;
-
-  // Get all the data from the form
-  var form = document.getElementById("photo_form");
-  var url = form.item_url.value;
-
-
-  if(url == " ")
+  //THIS IS THE ONE THAT WORKS
+  function addPhotoToDatabase()
   {
-    alert("Error: provide a value for each field");
-    return;
+    //Get user UID
+    var user = firebase.auth().currentUser;
+    var userID = user.uid;
+    //console.log(userID);
+    //Get user from the database
+    //var dbUser = firebase.database().ref('users').orderByChild('userID').equalTo(userID);
+    // Get all the data from the form
+    var form = document.getElementById("photo_form");
+    var url = form.item_url.value;
+    //var id = Math.floor(Math.random()*1000000);
+
+    if(url == " ")
+    {
+      alert("Error: provide a value for each field");
+      return;
+    }
+
+    firebase.database().ref('users').orderByChild('userID').equalTo(userID).set({
+      image: url
+    });
+    //displayPhoto()
   }
 
-firebase.database().ref('users/' + user).set({
-  link: url
-  });
-}
+  function addProfileToDatabase()
+  {
+    var user = firebase.auth().currentUser;
+    var userID = user.uid;
+    // Get all the data from the form
+    var form = document.getElementById("profile_form");
+    var fullName = form.fullName.value;
+    var birthDate = form.birthday.value;
+    var gender = form.gender.value;
+    var bio = form.bio.value;
+    console.log(userID);
+    console.log(fullName);
 
+    if(form == "" || fullName == "" || birthDate == "" || gender == "" || bio == "")
+    {
+      alert("Error: provide a value for each field");
+      return;
+    }
+  //var usersRef = firebase.database().ref.child('users');
+  firebase.database().ref.child('users').usersRef.push(
+  {
+    userID: userID,
+    fullName: fullName,
+    birthDate: birthDate,
+    gender: gender,
+    bio: bio
+    });
+  }
+//NOT SURE IF THIS ONE WORKS
+function displayPhoto()
+{
+  var userID = firebase.auth().currentUser.uid;
+  var user = firebase.database().ref('users').orderByChild('userID').equalTo('userID');
+  //firebase.database().ref('users').orderByChild('userID').equalTo(userID)
+  //var userID = currentUser.uid;
+  //var spaceRef = user.child('https://th.bing.com/th/id/OIP.rxfDXpkhzOPoaTdqYiqOTwHaE8?w=255&h=180&c=7&o=5&pid=1.7');
+  storageRef.child('image').getDownloadURL().then(function(url) {
+  var test = url;
+  alert(url);
+  document.querySelector('img').src = test;
+
+  }).catch(function(error) {
+          console.error(error);
+        });
+
+}
+// function displayPhoto()
+// {
+//   var newCard = document.createElement('div');
+//   newCard.classList = 'card';
+//
+// }
 /*function addProfileToDatabase()
 {
   var user = firebase.auth().currentUser;
-
   // Get all the data from the form
   var form = document.getElementById("");
   var url = form.item_url.value;
-
   if(url == " ")
   {
     alert("Error: provide a value for each field");
     return;
   }
-
 firebase.database().ref('users/' + user).set({
   fullName: fullName
   birthDate: birthday
@@ -108,10 +198,8 @@ firebase.database().ref('users/' + user).set({
   bio: bio
   });
 }
-
 function displayPhotos()
 {
-
 }*/
 /*.then(function() {
     console.log("Document successfully written!");
@@ -120,13 +208,10 @@ function displayPhotos()
 .catch(function(error) {
     console.error("Error writing document: ", error);
 });
-
 // Create the corresponding card
 createCard('card_' + id, itemName, url, descrip, quan, tags);
 }
-
 }
-
   //function that adds data from homepage.html to database
   function addMatchToDatabase()
   {
@@ -143,7 +228,6 @@ createCard('card_' + id, itemName, url, descrip, quan, tags);
       return;
     }
   }
-
   //function that adds data from preferencesettings.html to database
 /*  function addPreferencesToDatabase()
   {
@@ -160,7 +244,6 @@ createCard('card_' + id, itemName, url, descrip, quan, tags);
       return;
     }
   }
-
   //function that adds data from profilesettings.html to database
 /*  function addProfileToDatabase()
   {
@@ -177,7 +260,6 @@ createCard('card_' + id, itemName, url, descrip, quan, tags);
       return;
     }
   }
-
 //function to display profile on home page, called in onLoad
 /*function createCard(itemID, itemName, itemImg, itemDescrip, itemQuantity, itemTags)
 {
@@ -203,14 +285,11 @@ createCard('card_' + id, itemName, url, descrip, quan, tags);
     }
   }
 }
-
 //calls createCard and its passed the users array and takes data from each user to display in createCard
 function onLoad(users) {
   // TODO: Need to check for the user!
   // console.log(users);
-
   var user_keys = firebase.auth().users;
-
   for (i = 0; i < user_keys.length; i++) {
     var userID = user_keys[i];
     var userEmail = users[userID]['email'];
@@ -231,12 +310,7 @@ function onLoad(users) {
     }
   }
 }
-
-
-
-
 //BARTERHUB CODE USE FOR REFERENCES
-
 /*
 // FIREBASE CONFIG
 var firebaseConfig = {
@@ -252,15 +326,12 @@ var firebaseConfig = {
 // Initialize Firebase (this is standard, don't change these two lines)
 firebase.initializeApp(firebaseConfig);
 var firestore = firebase.firestore();
-
 const docRef = firestore.collection("inventory");
-
 // Array to hold all items that belong to the current user
 // var item_keys = [];
 var items = {};
 var itemToDelete;
 var prevNumCards = 0;
-
 // Stores all the user's new item in the database
 function addItemToDatabase()
 {
@@ -276,10 +347,8 @@ function addItemToDatabase()
     alert("Error: provide a value for each field");
     return;
   }
-
   // Other info that will be sent to the database (sold and date posted):
   var id = Math.floor(Math.random()*1000000);
-
   var item_tags = [];
   var noneChecked = true;
   if(form.foodBox.checked) { item_tags.push('food'); noneChecked = false;}
@@ -292,13 +361,11 @@ function addItemToDatabase()
   if(form.technologyBox.checked) { item_tags.push('technology'); noneChecked = false;}
   if(form.toolsBox.checked) { item_tags.push('tools'); noneChecked = false;}
   if(form.servicesBox.checked) { item_tags.push('services'); noneChecked = false;}
-
   if(noneChecked)
   {
     alert("Error: provide a value for each field");
     return;
   }
-
   // Add a new document in collection "inventory"
   firestore.collection("inventory").doc('card_' + id.toString()).set({
       date_posted: firebase.firestore.Timestamp.now(),
@@ -317,7 +384,6 @@ function addItemToDatabase()
   .catch(function(error) {
       console.error("Error writing document: ", error);
   });
-
   // Create the corresponding card
   createCard('card_' + id, itemName, url, descrip, quan, tags);
 }
@@ -345,7 +411,6 @@ function createCard(itemID, itemName, itemImg, itemDescrip, itemQuantity, itemTa
       <span class="label ${item_tags[i]}">${item_tags[i]}</span>`;
     }
   }
-
   // May need to add the <div class="col-auto mb-3"> to keep it in format
   var content = `
   <div id="${card_id}" class="card" style="width: 20rem;">
@@ -359,22 +424,18 @@ function createCard(itemID, itemName, itemImg, itemDescrip, itemQuantity, itemTa
     </div>
   </div>
   `;
-
   container.innerHTML += content;
 }
-
 // Handler that deals with remove item calls
 $(document).on("click", ".open-delete_item", function () {
      itemToDelete = $(this).data('id');
 });
-
 function removeCard()
 {
   var elem = document.getElementById(itemToDelete);
   elem.parentNode.removeChild(elem);
   return false;
 }
-
 function removeItemFromDatabase()
 {
   docRef.doc(itemToDelete).delete().then(function() {
@@ -385,7 +446,6 @@ function removeItemFromDatabase()
   });
   removeCard();
 }
-
 function successfulAdd(success)
 {
   if(success)
@@ -393,7 +453,6 @@ function successfulAdd(success)
     location.reload();
   }
 }
-
 getRealTimeUpdates = function(callback){
   docRef.onSnapshot({
     includeMetadataChanges: true
@@ -404,13 +463,10 @@ getRealTimeUpdates = function(callback){
       callback(items);
   });
 }
-
 function onLoad(items) {
   // TODO: Need to check for the user!
   // console.log(items);
-
   var item_keys = Object.keys(items);
-
   for (i = 0; i < item_keys.length; i++) {
     var itemID = item_keys[i];
     var itemEmail = items[itemID]['email'];
@@ -431,6 +487,5 @@ function onLoad(items) {
     }
   }
 }
-
 getRealTimeUpdates(onLoad);
 */
